17970fdfe90c91555a884b20cfec8079b2d6ba11
Alexander1248 <75137404+Alexander1248@users.noreply.github.com>
2022-03-20 13:04:33 +0300

    Delete src directory

--- src/main/java/ru/alexander1248/nnlib/NNHelpingTools.java
+++
@@ -1,9 +0,0 @@
-package ru.alexander1248.nnlib;
-
-public class NNHelpingTools {
-    private NNHelpingTools() {}
-
-    public static double trainingSpeedCalculator(double maxTSLimit, double forceCoefficient, double errorDelta) {
-        return maxTSLimit / (1 + Math.exp(1 - forceCoefficient * errorDelta));
-    }
-}
