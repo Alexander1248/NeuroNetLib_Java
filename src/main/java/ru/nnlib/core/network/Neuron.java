098911bfa20615ede98fe611f7aab63aad588046
Alexander1248 <sasha.izmyloff@mail.ru>
2022-03-28 23:41:05 +0300

    Neuron class

--- src/main/java/ru/nnlib/core/network/Neuron.java
+++ src/main/java/ru/nnlib/core/network/Neuron.java
@@ -1,4 +1,50 @@
 package ru.nnlib.core.network;
 
+import ru.nnlib.core.functions.ActivationFunction;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Neuron {
+    private final ActivationFunction function;
+
+    private final List<Double> inputs;
+    private final List<Double> weights;
+
+    private double output;
+
+    public Neuron(ActivationFunction function) {
+        this.function = function;
+        inputs = new LinkedList<>();
+        weights = new LinkedList<>();
+    }
+
+
+    public ActivationFunction getFunction() {
+        return function;
+    }
+
+    public double getOutput() {
+        return output;
+    }
+
+    public List<Double> getWeights() {
+        return weights;
+    }
+
+    public void setInput(int i, double val) {
+        inputs.set(i, val);
+    }
+
+    public void editWeight(int i, double delta) {
+        weights.set(i, weights.get(i) + delta);
+    }
+
+    public void addInput() {
+        inputs.add(0.0);
+        weights.add(Math.random() - 0.5);
+    }
+
+
 }
